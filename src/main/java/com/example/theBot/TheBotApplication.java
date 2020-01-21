e146a90f25144f001c600c0fd51a1f16add0eda7
synzen <michael994664@gmail.com>
2020-01-20 20:30:51 -0500

    Initial Commit

---
+++ src/main/java/com/example/theBot/TheBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.theBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/theBot/TheBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.theBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
