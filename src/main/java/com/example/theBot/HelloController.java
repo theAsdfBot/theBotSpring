6c62f2f51456287172b35f29e55b9990b1c02b20
synzen <michael994664@gmail.com>
2020-01-22 20:01:03 -0500

    Add product key model

--- src/main/java/com/example/theBot/HelloController.java
+++ src/main/java/com/example/theBot/HelloController.java
@@ -1,14 +1,28 @@
 package com.example.theBot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Autowired
+    private ProductKeyRepository keyRepository;
 
     @RequestMapping("/")
     public String index() {
         return "Greetings, tiny human!";
     }
 
+    @RequestMapping("/add")
+    public ProductKey createKey() {
+        ProductKey key = new ProductKey();
+        keyRepository.save(key);
+        return key;
+    }
+
+    @RequestMapping("/all")
+    public Iterable<ProductKey> getAllKeys() {
+        return keyRepository.findAll();
+    }
 }
---
+++ src/main/java/com/example/theBot/ProductKeyRepository.java
@@ -0,0 +1,6 @@
+package com.example.theBot;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductKeyRepository extends CrudRepository<ProductKey, Integer> {
+}
